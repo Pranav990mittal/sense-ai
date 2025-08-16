c23fc18f46dbeb6dd1068aca52f4f12869d9574a
Pranav990mittal <pranavmittal990@gmail.com>
2025-08-16 20:44:56 +0530

    Update layout.js

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,10 +1,17 @@
-import React from 'react'
+// import React from 'react'
 
-const MainLayout = ({children}) => {
-    // redirect to onboarding 
-  return (
-    <div className='container mx-auto mt-24 mb-20'>{children}</div>
-  )
-}
+// const MainLayout = ({children}) => {
+//     // redirect to onboarding 
+//   return (
+//     <div className='container mx-auto mt-24 mb-20'>{children}</div>
+//   )
+// }
 
-export default MainLayout
+// export default MainLayout
+import React from "react";
+
+const MainLayout = async ({ children }) => {
+  return <div className="container mx-auto mt-24 mb-20">{children}</div>;
+};
+
+export default MainLayout;
